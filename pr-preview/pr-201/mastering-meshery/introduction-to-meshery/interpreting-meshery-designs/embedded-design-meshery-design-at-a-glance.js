d949c083ec5710fb69ee666e7eefdae8d5137cb9
DHEVIKA <DHEVIKA@users.noreply.github.com>
2026-03-26 17:33:39 +0000

    Deploy preview for PR 201 🛫

---
+++ pr-preview/pr-201/mastering-meshery/introduction-to-meshery/interpreting-meshery-designs/embedded-design-meshery-design-at-a-glance.js
@@ -0,0 +1,7 @@
+const design = {
+  id: "bf3ef49f-0623-499b-b9e3-3b9e0da07557",
+  type: "meshery-design",
+  link: "https://kanvas.new/extension/meshmap?mode=design&design=bf3ef49f-0623-499b-b9e3-3b9e0da07557"
+}
+
+export default design;
