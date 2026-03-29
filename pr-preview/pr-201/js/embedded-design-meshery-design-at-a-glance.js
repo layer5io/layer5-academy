b8c6c43a2ff871c30b84fcfa28706c3d62fd6efb
saurabhraghuvanshii <saurabhraghuvanshii@users.noreply.github.com>
2026-03-29 12:47:06 +0000

    Remove preview for PR 201 🛬

--- pr-preview/pr-201/js/embedded-design-meshery-design-at-a-glance.js
+++
@@ -1,6 +0,0 @@
-// embedded-design-meshery-design-at-a-glance.js
-window.mesheryDesigns = window.mesheryDesigns || [];
-window.mesheryDesigns.push({
-  id: "bf3ef49f-0623-499b-b9e3-3b9e0da07557",
-  element: "meshery-design-embed"
-});
